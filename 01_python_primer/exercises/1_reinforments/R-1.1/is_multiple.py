ab38cd724ce99d2e45195fc22d113174ad624941
muwagamicheal <michealmuwaga@gmail.com>
2024-01-08 17:19:25 +0300

    added reinforcement exercise
    R-1.1

---
+++ 01_python_primer/exercises/1_reinforments/R-1.1/is_multiple.py
@@ -0,0 +1,7 @@
+n =input(int("Enter the number that i will check if its a multiple of the othet number you will provide be checked"))
+m = input(int("Enter the other number."))
+
+def is_multiple(n,m):{
+
+  }
+  
