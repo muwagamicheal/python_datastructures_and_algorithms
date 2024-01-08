3235f83c236384315afb045143a7b53106142b98
muwagamicheal <michealmuwaga@gmail.com>
2024-01-08 18:21:43 +0300

    Created level one solution
    task complete

--- 01_python_primer/exercises/1_reinforments/R-1.1/is_multiple.py
+++ 01_python_primer/exercises/1_reinforments/R-1.1/is_multiple.py
@@ -1,7 +1,21 @@
-n =input(int("Enter the number that i will check if its a multiple of the othet number you will provide be checked"))
-m = input(int("Enter the other number."))
+#n = int(input("Enter the number that i will check if its a multiple of the othet number you will provide be checked"))
 
-def is_multiple(n,m):{
+#m = int(input("Enter the other number."))
 
-  }
-  
+def is_multiple(n,m):
+  item = 1
+  multiples = []
+  #creating multiple of the given number
+  for item in range(1,m+1):
+    if m % item == 0:
+      multiples.append(item)
+    item += 1
+  print(multiples)
+
+  # Checking if the number is a multiple
+  if m%n == 0:
+    print(True)
+  else:
+    print(False, )
+
+is_multiple(2,150)
