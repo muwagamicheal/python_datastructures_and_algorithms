0fe1cc3954498e69e53c2623078d63c5b26488c7
muwagamicheal <michealmuwaga@gmail.com>
2024-01-09 07:22:30 +0300

    Created is_even2.py
    an also added solution

---
+++ 01_python_primer/exercises/1_reinforments/R-1.2/is_even2.py
@@ -0,0 +1,8 @@
+#Using conditional loops
+number = int(input("Enter an integer, I will tell you if its even or Not"))
+while(number >= 2):
+  number = number -2
+if number == 0:
+  print(True,"The number is Even")
+else:
+  print(False,"The number is odd")
