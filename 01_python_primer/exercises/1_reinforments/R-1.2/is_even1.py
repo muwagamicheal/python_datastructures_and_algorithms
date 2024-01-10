6884418aca91725f128ca96636bb8f8645329ae5
muwagamicheal <michealmuwaga@gmail.com>
2024-01-10 06:33:10 +0300

    created bitwise solution

--- 01_python_primer/exercises/1_reinforments/R-1.2/is_even1.py
+++ 01_python_primer/exercises/1_reinforments/R-1.2/is_even1.py
@@ -1,2 +1,9 @@
 # Here we are using the bitwise operator to check if the number is even or odd
 
+def is_even(k):
+    return k & 1 == 0
+
+# Test the function
+number = 19
+result = is_even(number)
+print(f"{number} is even: {result}") # here is use the f-string to pass varialble to the output.
