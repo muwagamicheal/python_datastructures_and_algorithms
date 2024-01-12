d3d563dc2cff5cd3312e3ecc1d6ce5f7d9d3d531
muwagamicheal <michealmuwaga@gmail.com>
2024-01-12 08:17:56 +0300

    Added comments to the code

--- 01_python_primer/exercises/1_reinforments/R-1.2/is_even3.py
+++ 01_python_primer/exercises/1_reinforments/R-1.2/is_even3.py
@@ -6,7 +6,9 @@ def is_even(k):
     binary_representation = bin(k)[2:]  # Convert to binary and remove '0b' prefix
     return bool(re.match(r'^.*0$', binary_representation))
 
-# Test the function
-number = int(input("Enter an interger \n"))
+# Capture user input
+number = input("Enter an interger \n")
+# pass it to the function and store the result in the varialble result.
 result = is_even(number)
+# Display the number entered by the user and the result.
 print(f"{number} is even: {result}")
